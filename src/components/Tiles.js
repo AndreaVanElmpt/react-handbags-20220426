b89ef76efb5f01dc8af36651b966960d366050e3
AndreaVanElmpt <oibibio@gmail.com>
2022-05-04 13:17:48 +0200

    laatse update

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -4,7 +4,7 @@ function Tiles({headline, image, brandTitle, children}) {
 
     return(
         <section>
-            <h2>{headline}</h2>
+            {headline && <h2>{headline}</h2>}
             {children}
             {image && <img src={image} alt={brandTitle} />}
         </section>
